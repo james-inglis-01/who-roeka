3661022baf5567c9e6f8998c95eb6e6945da6426
james-inglis-01 <james.a.i.inglis@gmail.com>
2017-07-28 15:53:52 +1200

    fixed footer position and changed id to className

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-
-
 class Footer extends React.Component {
-    constructor(props) {
-        super(props)
-        const state = this.props.score
-        console.log(state, this.state)
-    }
+  constructor (props) {
+    super(props)
+    const state = this.props.score
+    console.log(state, this.state)
+  }
 
-    render() {
-        return (
-            <div id='footer'>
-                <div id='footer-text'>
+  render () {
+    return (
+      <div className='footer'>
+        <div className='footer-text'>
                 Your Current Score: {this.props.score}
-                </div>                   
-            </div>
-        )
-    }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
--- public/bundle.js
+++ public/bundle.js
@@ -11513,7 +11513,7 @@ exports.default = FinalPage;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -11533,35 +11533,35 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Footer = function (_React$Component) {
-    _inherits(Footer, _React$Component);
+  _inherits(Footer, _React$Component);
 
-    function Footer(props) {
-        _classCallCheck(this, Footer);
+  function Footer(props) {
+    _classCallCheck(this, Footer);
 
-        var _this = _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).call(this, props));
 
-        var state = _this.props.score;
-        console.log(state, _this.state);
-        return _this;
+    var state = _this.props.score;
+    console.log(state, _this.state);
+    return _this;
+  }
+
+  _createClass(Footer, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'footer' },
+        _react2.default.createElement(
+          'div',
+          { className: 'footer-text' },
+          'Your Current Score: ',
+          this.props.score
+        )
+      );
     }
+  }]);
 
-    _createClass(Footer, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                { id: 'footer' },
-                _react2.default.createElement(
-                    'div',
-                    { id: 'footer-text' },
-                    'Your Current Score: ',
-                    this.props.score
-                )
-            );
-        }
-    }]);
-
-    return Footer;
+  return Footer;
 }(_react2.default.Component);
 
 exports.default = Footer;
